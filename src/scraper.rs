0e8550f1d836bde7f9676b266ebebc4840459a3f
root2116 <root6889@icloud.com>
2023-03-16 00:52:22 +0900

    thumbnail of the first image

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,7 +1,10 @@
 use scraper::Selector;
 use std::fs::{self, File};
 use std::io::{Write, BufWriter, BufRead, BufReader};
-
+use regex::Regex; 
+use base64::encode;
+use tokio::time;
+use bytes::Bytes;
 
 pub struct Scraper {
     base: String,
@@ -14,6 +17,14 @@ impl Scraper {
 
     pub fn new(base: &str, url: &str) -> Scraper {
         Scraper { base: base.to_string(), url: url.to_string() }
+    }
+
+    pub fn get_base(&self) -> &String {
+        &self.base
+    }
+
+    pub fn get_url(&self) -> &String {
+        &self.url
     }
 
     pub async fn scrape_latest_url(&self) -> String {
@@ -34,13 +45,60 @@ impl Scraper {
         return url;
     }
 
-    // pub async fn scrape_images(&self, url: String) {
+    fn extract_id(&self, url: &str) -> Option<String> {
+        let re = Regex::new(r"/(\d+)\?").unwrap();
+        re.captures(url).and_then(|cap| cap.get(1)).map(|m| m.as_str().to_owned())
+    }
 
-    // }
+    pub async fn scrape_images(&self, url: &str) -> Vec<Bytes> {
+
+
+        // let post_id = self.extract_id(url).unwrap();
+        let post_url: String;
+        if url.contains("https") {
+            post_url = String::from(url);
+        } else {
+            post_url = format!("{}{}",self.base, url);
+        }
+
+        let html = reqwest::get(post_url)
+        .await.unwrap().text().await.unwrap();
+        
+        let doc = scraper::Html::parse_document(&html);
+        let sel = Selector::parse("div.c-blog-article__text img").unwrap();
+        let mut images: Vec<Bytes> = vec![];
+
+        for (i, element) in doc.select(&sel).enumerate() {
+            let src = element.value().attr("src").unwrap();
+            println!("{}", src);
+            // let filename = format!("H{}-{:>02}.jpg",post_id, i+1);
+            let bytes = reqwest::get(src).await.unwrap()
+                                    .bytes().await.unwrap();
+            // let base64_str = encode(&bytes);
+
+            images.push(bytes);
+
+            time::sleep(time::Duration::from_millis(1000)).await;
+        }
+
+        return images;
+
+
+    }
+
+    
 
 
     pub async fn scrape_text(&self, url: &str) -> String {
-        let html = reqwest::get(format!("{}{}",self.base, url))
+
+        let post_url: String;
+        if url.contains("https") {
+            post_url = String::from(url);
+        } else {
+            post_url = format!("{}{}",self.base, url);
+        }
+
+        let html = reqwest::get(post_url)
         .await.unwrap().text().await.unwrap();
         
         let doc = scraper::Html::parse_document(&html);
--- src/twitter.rs
+++ src/twitter.rs
@@ -1,15 +1,19 @@
 use base64;
 use chrono::Utc;
 use reqwest;
+use reqwest::multipart;
 use reqwest::header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE};
 use percent_encoding::{utf8_percent_encode, AsciiSet};
+use serde::de::value;
 use std::collections::HashMap;
 use serde_json::Value;
 use std::fs::File;
+use std::io::Read;
 use std::io::{BufReader,Write, BufWriter};
 use chrono::{DateTime, Local};
 use serde::{Serialize, Deserialize};
 use serde_json::json;
+use bytes::Bytes;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Token {
@@ -31,29 +35,130 @@ struct ResponseToken {
 // Twitterの認証関連と一部ラッパー実装
 pub struct Twitter {
     client_id: String,
-    client_secret: String
+    client_secret: String,
+    consummer_key: String,
+    consummer_secret: String,
+    access_token_key: String,
+    access_token_secret: String,
+    user_id: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Image {
+    media_id_string : String
 }
 
 impl Twitter {
-   
+    const FRAGMENT: AsciiSet = percent_encoding::NON_ALPHANUMERIC
+        .remove(b'*')
+        .remove(b'-')
+        .remove(b'.')
+        .remove(b'_');
+
+
     // インスタンス生成
     pub fn new(
-        client_id: String, client_secret: String)
+        client_id: String, client_secret: String, consummer_key: String, consummer_secret: String, access_token_key: String, access_token_secret: String, user_id: String)
         -> Twitter {
         Twitter {
-            client_id, client_secret
+            client_id, client_secret, consummer_key, consummer_secret, access_token_key, access_token_secret, user_id
         }
     }
+    fn get_request_header(&self, method: &str, endpoint: &str) -> String {
+        let nonce = format!("nonce{}", Utc::now().timestamp());
+        let timestamp = format!("{}", Utc::now().timestamp());
+        // oauth_*パラメータ
+        let mut oauth_params: HashMap<&str, &str> = HashMap::new();
+        oauth_params.insert("oauth_consumer_key", &self.consummer_key);
+        oauth_params.insert("oauth_nonce", &nonce);
+        oauth_params.insert("oauth_signature_method", "HMAC-SHA1");
+        oauth_params.insert("oauth_timestamp", &timestamp);
+        oauth_params.insert("oauth_token", &self.access_token_key);
+        oauth_params.insert("oauth_version", "1.0");
 
-   
+        // シグネチャを計算
+        let oauth_signature = self.get_oauth_signature(
+            method, endpoint,
+            &self.consummer_secret, &self.access_token_secret,
+            &oauth_params);
+
+        // シグネチャをoauth_*パラメータに追加
+        oauth_params.insert("oauth_signature", &oauth_signature);
+
+        // ヘッダを返す
+        format!(
+            "OAuth {}",
+            oauth_params
+                .into_iter()
+                .map(|(key, value)| {
+                    format!(r#"{}="{}""#,
+                            utf8_percent_encode(key, &Self::FRAGMENT),
+                            utf8_percent_encode(value, &Self::FRAGMENT))
+                })
+                .collect::<Vec<String>>()
+                .join(", ")
+            )
+    }
+    
+    fn get_oauth_signature(
+        &self, method: &str, endpoint: &str,
+        consummer_secret: &str, access_token_secret: &str,
+        params: &HashMap<&str, &str>
+        ) -> String {
+
+        let key: String = format!("{}&{}",
+                                  utf8_percent_encode(consummer_secret, &Self::FRAGMENT),
+                                  utf8_percent_encode(access_token_secret, &Self::FRAGMENT));
+
+        let mut params: Vec<(&&str, &&str)> = params.into_iter().collect();
+        params.sort();
+
+        let param_string = params
+            .into_iter()
+            .map(|(key, value)| {
+                format!("{}={}",
+                        utf8_percent_encode(key, &Self::FRAGMENT),
+                        utf8_percent_encode(value, &Self::FRAGMENT))
+            })
+            .collect::<Vec<String>>()
+            .join("&");
+
+        let data = format!("{}&{}&{}",
+                           utf8_percent_encode(method, &Self::FRAGMENT),
+                           utf8_percent_encode(endpoint, &Self::FRAGMENT),
+                           utf8_percent_encode(&param_string, &Self::FRAGMENT));
+
+        let hash = hmacsha1::hmac_sha1(key.as_bytes(), data.as_bytes());
+
+        base64::encode(&hash)
+    }
 
     
 
-    pub async fn post(&self, text: String) -> reqwest::Result<()> {
+    pub async fn post(&self, text: String, images: &Vec<Bytes>) -> reqwest::Result<()> {
+
+        let mut media_ids: Vec<String> = vec![];
+        for image in images{
+            media_ids.push(self.upload_image(image).await);
+        }
+
+    
+        
         let client = reqwest::Client::new();
         let bearer_token = self.get_access_token().await.unwrap();
 
-        let post_data = json!({ "text" : text });
+        let mut post_data = json!({ "text" : text });
+        
+        if images.len() != 0 {
+            post_data = 
+            json!({ 
+                "text" : text,
+                "media" : {
+                    "media_ids" : media_ids[0..1]
+                } 
+            });
+        }
+        
 
      
         let res = client.post("https://api.twitter.com/2/tweets")
@@ -62,16 +167,85 @@ impl Twitter {
             .json(&post_data)
             .send()
             .await
-            .unwrap();
+            .unwrap().text().await.unwrap();
 
-        println!("{:?}",res);
+        println!("{}",res);
 
         Ok(())
 
 
     }
 
+    // image : base64 encode
+    async fn upload_image(&self, image: &Bytes) -> String {
+        let client = reqwest::Client::new();
+       
+        // let bearer_token = self.get_access_token().await.unwrap();
+        let endpoint = "https://upload.twitter.com/1.1/media/upload.json".to_string();
 
+        let header_auth = self.get_request_header("POST", &endpoint);
+        // println!("{}", header_auth);
+
+
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, header_auth.parse().unwrap());
+        // headers.insert(CONTENT_TYPE, "multipart/form-data".parse().unwrap());
+        
+
+        // let mut file = File::open("miku.jpg").unwrap();
+        // let mut buffer:Vec<u8> = Vec::new();
+        // file.read_to_end(&mut buffer).unwrap();
+
+        let part = multipart::Part::bytes(image.to_vec())
+        .file_name("image.jpg")
+        .mime_str("image/jpeg").unwrap();
+
+        // let part = multipart::Part::bytes(buffer).file_name("image.png");
+
+        let form = reqwest::multipart::Form::new()
+            .text("additional_owners", self.user_id.clone())
+            .part("media", part);
+
+        let res = client.post(&endpoint)
+            // .bearer_auth(bearer_token)
+            .headers(headers)
+            .multipart(form)
+            .send()
+            .await
+            .unwrap().text().await.unwrap();
+
+        
+            
+        
+
+        println!("{}", res);
+        
+        let image: Image = serde_json::from_str(&res).unwrap();
+
+        return image.media_id_string;
+    }
+
+    pub async fn get_user_id(&self, screen_name: &str) -> String {
+        let client = reqwest::Client::new();
+        let bearer_token = self.get_access_token().await.unwrap();
+        let endpoint = format!("https://api.twitter.com/1.1/users/show.json?screen_name={}",screen_name);
+        let header_auth = self.get_request_header("GET", &endpoint);
+
+        let mut headers = HeaderMap::new();
+        headers.insert(AUTHORIZATION, header_auth.parse().unwrap());
+
+
+        let res = client.get(endpoint)
+            .bearer_auth("AAAAAAAAAAAAAAAAAAAAAPyjmAEAAAAA7Xe%2BTE0Ep%2FDgM4CN0kOk%2F%2BSYu5A%3D0UqBlGrtq5IHADx4jkvj0KEYE8oZkgzvSgHCSI7ea5eftLeGPS")
+            // .headers(headers)
+            .send()
+            .await
+            .unwrap().text().await.unwrap();
+
+        println!("{}",res);
+        return String::new();
+
+    }
     #[allow(dead_code)]
     pub async fn get_access_token(&self) -> reqwest::Result<String> {
        
@@ -135,10 +309,5 @@ impl Twitter {
         
 
         
-        
-
-
-
-        return Ok(String::new());
     }
 }
