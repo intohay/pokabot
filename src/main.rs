114b58c0cdaccbd3edbeb9a4a6c4ffb98944bf72
root2116 <root6889@icloud.com>
2023-03-20 00:22:36 +0900

    add return in tweet_until_latest_post

--- src/main.rs
+++ src/main.rs
@@ -48,6 +48,11 @@ async fn tweet_until_latest_post(twitter: &Twitter, chatgpt: &ChatGPT, scraper: 
     let latest_post_id = helper::extract_post_id(&url).unwrap();
     let previous_post_id = helper::extract_post_id(&previous_url).unwrap();
 
+    if latest_post_id == previous_post_id {
+        return;
+    }
+
+    
     for id in previous_post_id..=latest_post_id{
         let target_url = format!("https://www.hinatazaka46.com/s/official/diary/detail/{}?ima=0000&cd=member", id);
         if scraper.page_exists(&target_url).await {
