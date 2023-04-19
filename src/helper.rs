8d26b23d22b49cd7beb35ab71caf3279e9c59e3c
root2116 <root6889@icloud.com>
2023-04-19 16:52:21 +0900

    tighten the twitter characters limit

--- src/helper.rs
+++ src/helper.rs
@@ -21,7 +21,7 @@ pub fn count_twitter_chars(text: &str) -> usize {
         }
 
 pub fn is_within_twitter_limit(text: &str) -> bool {
-    const TWITTER_LIMIT: usize = 280;
+    const TWITTER_LIMIT: usize = 260;
     count_twitter_chars(text) <= TWITTER_LIMIT
 }
 
@@ -32,7 +32,8 @@ mod tests {
 
     #[test]
     fn test_is_within_twitter_limit(){
-        // assert!(is_within_twitter_limit("山口陽世ちゃん、こんにちは！ブログ読んでファンになっちゃったよ☺️かき氷美味しそうだし、メキシコ戦感動的だったねOne choiceのフォーメーション楽しみにしてるよ！カラフルな衣装も可愛いし、まりぃちゃんのメガネ似合ってるねスパイファミリーのミュージカルも素晴らしかったんだね！リハ頑張ってね⚾️またブログ楽しみにしてるね\nhttps://www.hinatazaka46.com/s/official/diary/detail/49582?ima=0000&cd=member"));
+        assert!(is_within_twitter_limit("東村芽依ちゃんのブログ読んだよ！サインとメッセージ書かせてもらったんだね！｢One choice｣発売日おめでとう特に｢シーラカンス｣好きなんだって！4期生の透き通った歌声最高だね！世界卓球2023の応援サポーターに就任したって！小さい頃卓球好きだった
+からすごく嬉しいね\nhttps://www.hinatazaka46.com/s/official/diary/detail/50010?ima=0000&cd=member"));
         assert!(is_within_twitter_limit("山"));
 
     }
